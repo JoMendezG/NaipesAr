da6289d0fcd863a62beed381ac91ef4d2263b5b5
JoMendezG <37412612+JoMendezG@users.noreply.github.com>
2020-10-07 12:32:38 -0300

    Integración NaipeInglesAr
    Se integro el programa de Unity Naipe_Ingles_Ar al github. Este tiene avances en el trigger AR del led y la bateria.

---
+++ Naipe_Ingles_AR/Assets/Script/ledTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+
+public class ledTrigger : MonoBehaviour {
+    
+
+    public Light luzLed;
+
+    void Start() {
+        luzLed.enabled = false; 
+        Debug.Log("Inicio Código");
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+      if (other.gameObject.CompareTag("Bateria9V")) {
+            luzLed.enabled = true;  
+        } else {
+            luzLed.enabled = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        Debug.Log("Trigger desactivado");
+        luzLed.enabled = false; 
+    }
+
+
+}
---
+++ Naipe_Ingles_AR/Assets/Script/sueloAR.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class sueloAR : DefaultTrackableEventHandler
+{
+   
+    public Rigidbody rb;
+    public bool enSuelo;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        // Este base nos indica que llamamos al método
+        base.OnTrackingFound();
+
+       
+
+
+        // Agrego un código que permite escribir en la consola
+        Debug.Log ($"<color=green><b> Found! </b></color>");
+        
+        
+    }
+
+     protected override void OnTrackingLost()
+    {
+        // Aquí llamamos al método que heredamos del código original
+        base.OnTrackingLost();
+
+        // Agrego un código que permite escribir en la consola
+        Debug.Log($"<color=red><b> LOST! </b></color>");
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("enSuelo"))
+        {
+            enSuelo = true;
+        }
+    }
+
+}
