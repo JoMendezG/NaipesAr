23e331f097c23626409c721bc720435cb1c87ff0
JoMendezG <37412612+JoMendezG@users.noreply.github.com>
2020-10-07 12:39:39 -0300

    Integración Trigger
    Se integra el archivo de Unity que almacena la información como realizar un trigger pero no AR junto con tags establecidos.

---
+++ trigger/Assets/suelo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class suelo : MonoBehaviour
+{
+    
+    public Rigidbody rb;
+    public bool enSuelo;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("enSuelo"))
+        {
+            enSuelo = true;
+        }
+    }
+
+}
---
+++ trigger/Assets/triggerLed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerLed : MonoBehaviour
+{
+public Light luzLed;
+
+    void Start()
+    {
+         luzLed.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Bateria9V")) {
+            luzLed.enabled = true;  
+        } else {
+            luzLed.enabled = false;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other) {
+        luzLed.enabled = false;
+    }
+
+}
